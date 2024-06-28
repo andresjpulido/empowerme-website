d4e68fa2ddc5379fc643924d208da9eba6fddb35
Andres <andresjpulido@gmail.com>
2024-06-28 12:21:22 +1200

    setting base in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://empowermecollective.com",
+  base: '/' ,
   integrations: [mdx(), sitemap(), tailwind({applyBaseStyles: false})],
 });
