e9de129c9a13b1ad84225fa6bf399bd7e480f457
Andres <andresjpulido@gmail.com>
2024-08-20 22:46:42 +1200

    adding answers on Q&A section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,8 @@ export default {
       nine: "#a7b9c3", //clear fg
       tenth: "#2D6873",
       eleventh: "#D1E4E8",
+      accordionbg: "#F5F5F5",
+      accordionborder: "#D9D9D9",
       white: "red",
       "blue-300": "blue",
       "blue-600": "blue",
