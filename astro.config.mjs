491bb065ed2b92c177da02d924afb6a1d6dc7cb0
Andres <andresjpulido@gmail.com>
2024-09-12 12:56:51 +1200

    remove www in site attribute

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.empowermecollective.com",
+  site: "https://empowermecollective.com",
   base: '/' ,
   integrations: [mdx(), sitemap(), tailwind({applyBaseStyles: false})],
 });
