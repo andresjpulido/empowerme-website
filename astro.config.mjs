5c05eb444e7a27b4b9d7f55fc7b24abac61d37ee
Andres <andresjpulido@gmail.com>
2025-09-27 13:09:54 +1200

    removing sitemap links

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,13 +14,13 @@ export default defineConfig({
     sitemap({
       filter: (page) =>
         !page.startsWith("https://empowermecollective.com/blog/")  &&
-        !page.startsWith("https://empowermecollective.com/courses")  /*&&
+        !page.startsWith("https://empowermecollective.com/courses") &&
         page !== "https://empowermecollective.com/termsofuse/" &&
         page !== "https://empowermecollective.com/klaviyo/"  && 
         page !== "https://empowermecollective.com/privacypolicy/"  && 
         page !== "https://empowermecollective.com/termsofuse/" && 
         page !== "https://empowermecollective.com/about/" &&
-        page !== "https://empowermecollective.com/letterOfFuture"*/
+        page !== "https://empowermecollective.com/letterOfFuture"
     }), 
     tailwind({applyBaseStyles: false}),
   
